30d468a853ee520f3fa76048e17f31b16c7deff5
FiveM RealWorld <66913632+fivem-realw@users.noreply.github.com>
2020-09-01 07:43:06 +0900

    new

---
+++ __resource.lua
@@ -0,0 +1,34 @@
+---------------------------------------------------------
+-------------- RealWorld - VRP UserList -----------------
+-------------- https://discord.gg/realw -----------------
+---------------------------------------------------------
+
+resource_manifest_version "44febabe-d386-4d18-afbe-5e627f4af937"
+
+description "VRP Scoreboard"
+
+version "1.0.0"
+
+dependency "vrp"
+
+shared_scripts {
+	"@evp/main.lua",
+	"@vrp/lib/utils.lua",
+	"config.lua"
+}
+
+client_scripts {
+	"@vrp/client/Tunnel.lua",
+	"@vrp/client/Proxy.lua",
+	"client.lua"
+}
+
+server_scripts {
+	"server.lua"
+}
+
+ui_page "html/ui.html"
+
+files {
+	"html/*.*"
+}
---
+++ config.lua
@@ -0,0 +1,37 @@
+---------------------------------------------------------
+-------------- RealWorld - VRP UserList -----------------
+-------------- https://discord.gg/realw -----------------
+---------------------------------------------------------
+
+Config = {}
+Config.jobIcons = {
+	["person"] = "👱",
+	["newbie"] = "🌱",
+	["ems"] = "🚑",
+	["cop"] = "👮🏻",
+	["uber"] = "🚖",
+	["repair"] = "🔧",
+	["shh"] = "⚔",
+	["mafia"] = "💀",
+	["gm"] = "🚧",
+	["tow"] = "⛄",
+	["kys"] = "👔",
+	["helper"] = "🐥",
+	["inspector"] = "🔰",
+	["admin"] = "🌟"
+}
+Config.categories = {
+	["all"] = {1, "전체", "👨‍⚕️"},
+	["ems"] = {2, "EMS", Config.jobIcons.ems},
+	["cop"] = {3, "경찰", Config.jobIcons.cop},
+	["uber"] = {4, "택시", Config.jobIcons.uber},
+	["repair"] = {5, "정비공", Config.jobIcons.repair},
+	["shh"] = {6, "흑사회", Config.jobIcons.shh},
+	["mafia"] = {7, "백사회", Config.jobIcons.mafia},
+	["gm"] = {8, "퍼스트렉카", Config.jobIcons.gm},
+	["tow"] = {9, "리얼다이소", Config.jobIcons.tow},
+	["kys"] = {10, "교정본부", Config.jobIcons.kys},
+	["helper"] = {11, "스태프", Config.jobIcons.helper},
+	["admin"] = {12, "관리자", Config.jobIcons.admin}
+}
+Config.toggleKey = {289, 113}
